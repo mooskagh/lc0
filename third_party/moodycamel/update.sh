615984f741994b08be20a0cc54c7923507a3f6aa
Alexander Lyashuk <crem@google.com>
2025-04-19 12:05:17 +0200

    moodycamel queues

---
+++ third_party/moodycamel/update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+curl -O https://raw.githubusercontent.com/cameron314/concurrentqueue/refs/heads/master/concurrentqueue.h
+curl -O https://raw.githubusercontent.com/cameron314/concurrentqueue/refs/heads/master/lightweightsemaphore.h
+curl -O https://raw.githubusercontent.com/cameron314/concurrentqueue/refs/heads/master/blockingconcurrentqueue.h
+curl -O https://raw.githubusercontent.com/cameron314/readerwriterqueue/refs/heads/master/readerwriterqueue.h
+curl -O https://raw.githubusercontent.com/cameron314/readerwriterqueue/refs/heads/master/readerwritercircularbuffer.h
+curl -O https://raw.githubusercontent.com/cameron314/readerwriterqueue/refs/heads/master/atomicops.h
+curl -o LICENSE-concurrentqueue.md https://raw.githubusercontent.com/cameron314/concurrentqueue/refs/heads/master/LICENSE.md
+curl -o LICENSE-readwriterqueue.md https://raw.githubusercontent.com/cameron314/readerwriterqueue/refs/heads/master/LICENSE.md
