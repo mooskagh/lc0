dfacf837225f0f434b98d64d15cdb5bd5e7d622f
Alexander Lyashuk <crem@google.com>
2025-04-27 14:55:04 +0200

    Make freelist thread-safe

--- src/utils/freelist.h
+++ src/utils/freelist.h
@@ -8,7 +8,8 @@ namespace lczero {
 template <typename T, size_t BlockSize>
 class FreeList {
  public:
-  FreeList() { AddNewBlock(); }
+  FreeList() = default;
+  ~FreeList() = default;
   FreeList(const FreeList&) = delete;
   FreeList& operator=(const FreeList&) = delete;
   FreeList(FreeList&&) = delete;
@@ -16,23 +17,71 @@ class FreeList {
 
   template <typename... Args>
   [[nodiscard]] T* New(Args&&... args) {
-    if (!head_) [[unlikely]] {
-      AddNewBlock();
+    Node* node = Pop();
+    while (!node) {
+      AllocateNewBlock();
+      node = Pop();
     }
-    Node* node = head_;
-    head_ = node->next;
-    return ::new (static_cast<void*>(node->storage))
-        T(std::forward<Args>(args)...);
+    T* obj_ptr = reinterpret_cast<T*>(node);
+    ::new (obj_ptr) T(std::forward<Args>(args)...);
+    return obj_ptr;
   }
 
   void Release(T* p) noexcept(std::is_nothrow_destructible_v<T>) {
-    p->~T();
+    if constexpr (!std::is_trivially_destructible_v<T>) p->~T();
     Node* node = reinterpret_cast<Node*>(p);
-    node->next = head_;
-    head_ = node;
+    Push(node);
   }
 
  private:
+  Node* Pop() noexcept {
+    Node* current_head = head_.load(std::memory_order_acquire);
+    while (current_head) {
+      Node* next_node = current_head->next;
+      if (head_.compare_exchange_weak(current_head, next_node,
+                                      std::memory_order_release,
+                                      std::memory_order_acquire)) {
+        return current_head;
+      }
+    }
+    return nullptr;
+  }
+
+  void Push(Node* node) noexcept {
+    Node* current_head = head_.load(std::memory_order_acquire);
+    do {
+      node->next = current_head;
+    } while (!head_.compare_exchange_weak(current_head, node,
+                                          std::memory_order_release,
+                                          std::memory_order_acquire));
+  }
+
+  void AllocateNewBlock() {
+    auto new_block_ptr = std::make_unique<Block>();
+    Block* block_raw_ptr = new_block_ptr.get();
+
+    {
+      std::lock_guard<std::mutex> lock(block_mutex_);
+      if (head_.load(std::memory_order_relaxed) != nullptr) return;
+      buffers_.push_back(std::move(new_block_ptr));
+    }
+
+    Node* block_head = &block_raw_ptr->nodes[0];
+    Node* block_tail = &block_raw_ptr->nodes[BlockSize - 1];
+
+    for (size_t i = 0; i < BlockSize - 1; ++i) {
+      block_raw_ptr->nodes[i].next = &block_raw_ptr->nodes[i + 1];
+    }
+    block_raw_ptr->nodes[BlockSize - 1].next = nullptr;
+
+    Node* current_head = head_.load(std::memory_order_acquire);
+    do {
+      block_tail->next = current_head;
+    } while (!head_.compare_exchange_weak(current_head, block_head,
+                                          std::memory_order_release,
+                                          std::memory_order_acquire));
+  }
+
   union Node {
     static constexpr size_t StorageSize = std::max(sizeof(T), sizeof(Node*));
     static constexpr size_t StorageAlign = std::max(alignof(T), alignof(Node*));
@@ -45,18 +94,9 @@ class FreeList {
     Node nodes[BlockSize];
   };
 
-  std::vector<std::unique_ptr<Block>> buffers_;
-  Node* head_ = nullptr;
-
-  void AddNewBlock() {
-    auto new_block = std::make_unique<Block>();
-    for (size_t i = 0; i < BlockSize; ++i) {
-      Node* current = std::addressof(new_block->nodes[BlockSize - 1 - i]);
-      current->next = head_;
-      head_ = current;
-    }
-    buffers_.push_back(std::move(new_block));
-  }
+  std::atomic<Node*> head_{nullptr};
+  std::vector<std::unique_ptr<Block>> buffers_;  // absl REQUIRES(block_mutex_);
+  std::mutex block_mutex_;
 };
 
 }  // namespace lczero
