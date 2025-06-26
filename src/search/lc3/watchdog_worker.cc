2ff161ea7e7503d5cc6008909d4ca2eee706c7f7
Alexander Lyashuk <crem@google.com>
2025-06-26 22:16:18 +0200

    Build PV from nodes

--- src/search/lc3/watchdog_worker.cc
+++ src/search/lc3/watchdog_worker.cc
@@ -61,25 +61,45 @@ void WatchdogWorker::CheckOnce() {
 
 std::vector<Move> WatchdogWorker::BuildPV() const {
   std::vector<Move> pv;
-  NodeKey current_hash = (*ctx_.head)->key;
-  Position current_position = (*ctx_.head)->position;
+  struct HashAndPosition {
+    NodeKey hash;
+    std::vector<Move> moves = {};
+    size_t n = 0;
+  };
 
-  while (NodeHandle node_handle = ctx_.node_repository->GetNodeForUpdate(
-             current_hash,
-             /*create_if_missing=*/false)) {
-    const size_t num_moves = node_handle.FetchMoveCounts().with_visits;
-    if (num_moves == 0) break;
-    std::vector<Move> moves(num_moves);
-    std::vector<uint64_t> n(num_moves);
-    node_handle.FetchEdges({.moves = moves, .n = n});
-    node_handle.Release();
-    const size_t best_idx = std::max_element(n.begin(), n.end()) - n.begin();
-    const Move best_move = moves[best_idx];
-    pv.push_back(best_move);
-    current_position = Position(current_position, best_move);
-    current_hash = NodeKey{HashCat(current_hash.hash, best_move.raw_data())};
+  auto fetch_position = [&](const NodeKey& key) -> HashAndPosition {
+    NodeHandle node_handle =
+        ctx_.node_repository->GetNodeForUpdate(key,
+                                               /*create_if_missing=*/false);
+    if (!node_handle) return HashAndPosition{};
+    size_t num_moves = node_handle.FetchMoveCounts().with_visits;
+    HashAndPosition current{
+        .hash = key,
+        .moves = std::vector<Move>(num_moves),
+        .n = node_handle.GetNodeAggregates().n,
+    };
+    node_handle.FetchEdges(
+        NodeHandle::EdgeDataDestination{.moves = current.moves});
+    return current;
+  };
+
+  std::optional<HashAndPosition> current_position =
+      fetch_position((*ctx_.head)->key);
+
+  while (current_position && !current_position->moves.empty()) {
+    std::vector<HashAndPosition> candidates;
+    for (const Move& move : current_position->moves) {
+      NodeKey next_hash{HashCat(current_position->hash.hash, move.raw_data())};
+      candidates.push_back(fetch_position(next_hash));
+    }
+    size_t best_idx =
+        std::max_element(candidates.begin(), candidates.end(),
+                         [](const HashAndPosition& a,
+                            const HashAndPosition& b) { return a.n < b.n; }) -
+        candidates.begin();
+    pv.push_back(current_position->moves[best_idx]);
+    current_position = std::move(candidates[best_idx]);
   }
-
   return pv;
 }
 }  // namespace lc3
